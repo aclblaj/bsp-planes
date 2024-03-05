df9cdcdd66e733cc6696a258fbf334a5eb980951
aclblaj <10652953+aclblaj@users.noreply.github.com>
2024-03-05 16:29:39 +0100

    removed comments

--- UdpClient.java
+++ UdpClient.java
@@ -6,7 +6,6 @@ public class UdpClient {
   public static void main(String[] args) {
     try { 
 	
-	  // InetAddress iaddr = InetAddress.getByName( "10.0.2.15" );
 	  InetAddress iaddr = InetAddress.getLocalHost();
 	
       DatagramPacket dp = new DatagramPacket(new byte[20], 20, iaddr, 55000);
@@ -21,12 +20,7 @@ public class UdpClient {
 		  
 		  ds.receive(dp);
 		  System.out.println(new String(dp.getData()));
-	  }
-	  /*ds.send(dp);
-      ds.receive(dp);
-      System.out.println("S2." + new String(dp.getData()));
-	  */
-	  
+	  }	  
 	  
 	} catch (UnknownHostException e) { e.printStackTrace();}
 	  catch (SocketException e) { e.printStackTrace();}
